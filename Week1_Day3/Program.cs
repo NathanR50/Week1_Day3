2e2819b7ae66b1d2cc20f7a926f0140e8ead3c63
NathanR50 <nathanratner42@gmail.com>
2017-09-27 12:05:31 -0400

    third commit

--- Week1_Day3/Program.cs
+++ Week1_Day3/Program.cs
@@ -10,6 +10,46 @@ namespace Week1_Day3
     {
         static void Main(string[] args)
         {
+
+            /* int firstNumber = 6;
+             int secondNumber = 2;
+
+             if(firstNumber > secondNumber)
+             {
+                 Console.WriteLine("You have broken math");
+             }
+
+             {
+                 Console.WriteLine("That makes sense");
+             }*/
+
+            {
+
+                Console.WriteLine("what day is today?");
+                string userAnswer = (Console.ReadLine);
+
+                if (userAnswer == "Monday")
+                            {
+
+                                Console.WriteLine("Back to work");
+
+                            }
+           
+                 else if (userAnswer == "Friday")
+
+                            {
+                                Console.WriteLine("TGIF!");
+                            }
+                else
+                {
+                    Console.WriteLine("Bloop!");
+
+
+                }
+
+
+            }
+
         }
     }
 }
